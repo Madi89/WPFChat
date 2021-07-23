1ba660f90331285b85746e75eef2a17fe58687d4
CC-Student <CC-Student@MININT-QF32BJG>
2021-07-23 15:23:24 +0200

    Projektdateien hinzufügen.

---
+++ WPFChat/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+
+
+namespace WPFChat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnHelp(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Button_ClickLogin(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Reg(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WPFChat/PageHelp.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Windows.Controls;
+
+namespace WPFChat
+{
+    /// <summary>
+    /// Interaktionslogik für PageHelp.xaml
+    /// </summary>
+    public partial class PageHelp : Page
+    {
+        public PageHelp()
+        {
+            InitializeComponent();
+        }
+    }
+}
